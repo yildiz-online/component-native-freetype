6648c793a16114e791a7f9a02188d1202f68e3d4
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-03 17:32:56 +0100

    [FIX] build.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,4 +1,6 @@
-cmake ../../c++ \
+cp -R ../../c++ .
+
+cmake . \
 -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=TRUE \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
@@ -6,16 +8,5 @@ cmake ../../c++ \
 
 make install
 r1=$?
-make clean
-
-rm -R CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-rm install_manifest.txt
-rm -R include
-rm CPackConfig.cmake
-rm CPackSourceConfig.cmake
-rm -R ../../../../target/classes/linux64/lib/cmake
 
 return $r1
--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,23 +1,12 @@
-cmake ../../c++ \
+cp -R ../../c++ .
+
+cmake . \
 -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=TRUE \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
-cp "../../c++/include/freetype/config/ftconfig.h" "freetype/config/ftconfig.h"
-
 make install
 r1=$?
-make clean
-
-rm -R CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-rm install_manifest.txt
-rm -R include
-rm CPackConfig.cmake
-rm CPackSourceConfig.cmake
-rm libfreetype.a
 
 return $r1
