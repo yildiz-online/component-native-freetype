3171a3c1a4fd0a7a650b3d0a32bd0e69f08fb7cd
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-03 16:49:11 +0100

    [FIX] Add missing file.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,7 +1,14 @@
-cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+cmake ../../c++ \
+-DCMAKE_BUILD_TYPE=Release \
+-DBUILD_SHARED_LIBS=TRUE \
+-DCMAKE_INSTALL_PREFIX="../../../../target/classes/win64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+cp "../../c++/include/freetype/config/ftconfig.h" "freetype/config/ftconfig.h"
 
 make install
 r1=$?
+make clean
 
 rm -R CMakeFiles
 rm CMakeCache.txt
