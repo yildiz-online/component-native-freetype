2c168392922da5a9f9e682bf2616c6475111b78a
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-04 21:57:25 +0100

    [TASK] Support png.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,6 +1,10 @@
 cmake ../../c++ \
 -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=TRUE \
+-DZLIB_LIBRARY="${PWD}/zlib/linux64/lib/libz.a" \
+-DZLIB_INCLUDE_DIR="${PWD}/zlib/linux64/include" \
+-DPNG_LIBRARY="${PWD}/png/linux64/lib/libpng.a" \
+-DPNG_PNG_INCLUDE_DIR="${PWD}/png/linux64/include" \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
 -G "Unix Makefiles"
 
