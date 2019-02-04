271649d962444bc89ed900acc4e9e50e43def919
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-04 22:16:16 +0100

    [TASK] cleaning.

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,8 +1,6 @@
 cmake ../../c++ \
 -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=TRUE \
--DPNG_LIBRARY="${PWD}/png/linux64/lib/libpng.a" \
--DPNG_PNG_INCLUDE_DIR="${PWD}/png/linux64/include" \
 -DCMAKE_INSTALL_PREFIX="../../../../target/classes/linux64" \
 -G "Unix Makefiles"
 
